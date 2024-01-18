c381712b0783f40ac9c0ece435cc6f994535d119
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-01-18 01:47:02 +0000

    Update scan.py

--- scan.py
+++ scan.py
@@ -161,8 +161,6 @@ async def main():
 
 if __name__ == '__main__':
     try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(main())
+        asyncio.get_event_loop().run_until_complete(main())
     except KeyboardInterrupt:
         exit()
